fbaad1402a7381d613a02e1cc6f1e2ff3203fb2c
Zhaokai Guan <zguan25@myseneca.ca>
2024-08-07 10:31:38 -0400

    NavBar and Quote Button completed. Moving onto Body

---
+++ src/components/ui/GetQuoteButton/GetQuoteButton.tsx
@@ -0,0 +1,13 @@
+import { Button } from 'react-bootstrap';
+import './GetQuoteButton.css';
+
+
+const GetQuoteButton: React.FC = ()=>{
+    return (
+        <Button className='Quote-Button'>
+            Get an Online Quote Now!
+        </Button>
+    );
+}
+
+export default GetQuoteButton;
